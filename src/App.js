00314e4ff7a730543ddc1b97860eec9d1888efe3
JanGospodarek <jan.gospo@onet.pl>
2022-07-11 10:43:21 +0200

    redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
-
+import UserProfile from "./components/UserProfile";
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuth);
   return (
     <React.Fragment>
       <Header></Header>
-      <Auth></Auth>
+      {!isAuth ? <Auth></Auth> : <UserProfile></UserProfile>}
       <Counter />
     </React.Fragment>
   );
